5d8ae7c6ecd31dc97905eb716e264c6eed0fcc79
Vincency <vincentokorike@gmail.com>
2025-03-19 07:00:30 +0100

    further modification

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import './App.css'
 import CreateAccount from "./pages/CreateAccount";
 import VerifyPage from "./pages/VerifyPage";
-import Home from "./pages/Home";
+import Home from "./pages/2Home";
 import CreatePassword from "./pages/CreatePassword";
 import EmailResetLink from "./pages/EmailResetLink";
 import ResetPassword from "./pages/ResetPassword";
 import Login from "./pages/Login";
+import StaysSearchResults from "./pages/StaysSearchResults";
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Route path="/create-password" element={<CreatePassword />} />
         <Route path="/reset-email-link" element={<EmailResetLink />} />
         <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/stays-search-result" element={<StaysSearchResults />} />
       </Routes>
   );
 }
